3a0100f4e9138d995742febf7e6ffb44a044f4c9
Stephen Healey <s_healey@hotmail.co.uk>
2021-01-01 17:07:13 +0000

    Added seven segment display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SafeDialComponent } from './safe-dial/safe-dial.component';
+import { SevenSegmentDisplayComponent } from './seven-segment-display/seven-segment-display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SafeDialComponent
+    SafeDialComponent,
+    SevenSegmentDisplayComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/seven-segment-display/seven-segment-display.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-seven-segment-display',
+  templateUrl: './seven-segment-display.component.html',
+  styleUrls: ['./seven-segment-display.component.scss']
+})
+export class SevenSegmentDisplayComponent implements OnInit, OnChanges {
+
+  @Input() number: number;
+
+  public matrix = [
+    [true, true, true, true, true, true, false],
+    [false, true, true, false, false, false, false],
+    [true, true, false, true, true, false, true],
+    [true, true, true, true, false, false, true],
+    [false, true, true, false, false, true, true],
+    [true, false, true, true, false, true, true],
+    [true, false, true, true, true, true, true],
+    [true, true, true, false, false, false, false],
+    [true, true, true, true, true, true, true],
+    [true, true, true, true, false, true, true],
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.number.currentValue > 9) {
+      this.number = 9;
+    }
+  }
+
+}
