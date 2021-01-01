bac2fc2c7ff53d7d01378d8d7d236e18c36b234b
Stephen Healey <s_healey@hotmail.co.uk>
2021-01-01 10:08:43 +0000

    Removed seven-segment-display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { SevenSegmentDisplayComponent } from './seven-segment-display/seven-segment-display.component';
 import { SafeDialComponent } from './safe-dial/safe-dial.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SevenSegmentDisplayComponent,
     SafeDialComponent
   ],
   imports: [
